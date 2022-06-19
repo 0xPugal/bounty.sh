ea4c747fe5f71ca0f0389fd5f3ed980e84288b49
Pugalarasan <75373225+0xlittleboy@users.noreply.github.com>
2022-06-19 21:54:08 +0530

    Update bounty.sh

--- bounty.sh
+++ bounty.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
-echo "                                                                  "
-echo "   /$$                                       /$$                  "
-echo "  | $$                                      | $$                  "
-echo "  | $$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$  /$$$$$$   /$$   /$$    "
-echo "  | $$__  $$ /$$__  $$| $$  | $$| $$__  $$|_  $$_/  | $$  | $$    "
-echo "  | $$  \ $$| $$  \ $$| $$  | $$| $$  \ $$  | $$    | $$  | $$    "
-echo "  | $$  | $$| $$  | $$| $$  | $$| $$  | $$  | $$ /$$| $$  | $$    "
-echo "  | $$$$$$$/|  $$$$$$/|  $$$$$$/| $$  | $$  |  $$$$/|  $$$$$$$    "
-echo "  |_______/  \______/  \______/ |__/  |__/   \___/   \____  $$    "
-echo "                                                     /$$  | $$    "
-echo "                                                    |  $$$$$$/    "
-echo "                                                     \______/     "
+sleep 1
+cat <<"EOF"
+ /===============================================================\
+||   /$$                                       /$$               ||      
+||  | $$                                      | $$               ||
+||  | $$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$  /$$$$$$   /$$   /$$ ||
+||  | $$__  $$ /$$__  $$| $$  | $$| $$__  $$|_  $$_/  | $$  | $$ ||
+||  | $$  \ $$| $$  \ $$| $$  | $$| $$  \ $$  | $$    | $$  | $$ ||
+||  | $$  | $$| $$  | $$| $$  | $$| $$  | $$  | $$ /$$| $$  | $$ ||
+||  | $$$$$$$/|  $$$$$$/|  $$$$$$/| $$  | $$  |  $$$$/|  $$$$$$$ ||
+||  |_______/  \______/  \______/ |__/  |__/   \___/   \____  $$ ||
+||                                                     /$$  | $$ ||
+||                                                    |  $$$$$$/ ||
+||                                                     \______/  ||
+||                                                               ||
+||                                                               ||
+||         Developed by Pugalarasan - aka @litt1eb0y "           ||
+||      Run this script against your target to earn bounties"    ||
+ \===============================================================/
+EOF
 
-echo "          Developed by Pugalarasan - aka @litt1eb0y "
-echo "        Run this script against your target to earn $$$$"
-###############################################################################################################################
+echo ""
+echo ""
+sleep 1
+##############################################################################################################################
+
 #SubDomain Enumeration Started...
 echo "...Subdomain enumeration started..."
 
@@ -29,17 +39,24 @@ subfinder -d $1 -pc ~/.config/subfinder/provider-config.yaml -o subfinder.txt
 
 #Findomain started...
 findomain-linux -t $1 --quiet | tee -a findomain.txt
-
+echo ""
+echo ""
 #Removing Duplicates && Sorting 
 echo "...Sorting the subdomains..."
 cat assetfinder.txt amass.txt subfinder.txt findomain.txt | sort -u | tee -a all.txt
 rm assetfinder.txt amass.txt subfinder.txt findomain.txt
-
+echo ""
+echo ""
+sleep 1
 #Checking for live domains
 echo "...Checking for Live Domains..."
 httpx -l all.txt -p 80,8080,443,8443,9000,8000 -o alive.txt
+echo ""
 
 #############################################################################################################################
+
+echo ""
+sleep 1
 #Vulnerability Scanning - Nuclei...
 echo "...Nuclei started..."
 nuclei -l alive.txt -severity low -o nuclei-low.txt &&
@@ -47,18 +64,30 @@ nuclei -l alive.txt -severity low -o nuclei-low.txt &&
         nuclei -l alive.txt -severity high -o nuclei-high.txt &&
         nuclei -l alive.txt -severity critical -o nuclei-critical.txt &&
         nuclei -l alive.txt -severity info -o nuclei-info.txt
+echo ""
 
 ################################################################################################################################
+
+echo ""
+sleep 1
 #Fuzzing started - ffuf...
 echo "...Fuzzing started..."
 for URL in $(<alive.txt); do ( ffuf -u "${URL}/FUZZ" -w wordlists.txt -v -mc 200 -ac ); done | tee -a fuff.txt
+echo ""
 
 ############################################################################################################################
+
+echo ""
+sleep 1
 #PortScanning started...
 echo "...Portscan - Naabu..."
 naabu -l all.txt -p 1-65535 -o portscan.txt
+echo ""
 
 #################################################################################################################################
+
+echo ""
+sleep 1
 #Gathering URLs....
 echo "...Gathering URLs - Waybackurls,Gau,Gauplus..."
 cat alive.txt | waybackurls | uro | tee -a way.txt &&
@@ -66,8 +95,12 @@ cat alive.txt | gauplus | uro | tee -a gaupl.txt &&
 cat alive.txt | gau | uro | tee -a gau.txt &&
 cat way.txt gaupl.txt gau.txt | sort -u | tee -a waybackurls.txt
 rm way.txt gaupl.txt gau.txt
+echo ""
 
 #################################################################################################################################
+
+echo ""
+sleep 1
 #Gathering JS Files...
 echo "...Gathering JS Files..."
 cat alive.txt | waybackurls | grep -iE '\.js'|grep -iEv '(\.jsp|\.json)' >> js1.txt &&
@@ -75,5 +108,10 @@ cat alive.txt | waybackurls | grep -iE '\.js'|grep -iEv '(\.jsp|\.json)' >> js1.
         cat alive.txt | gau | grep -iE '\.js'|grep -iEv '(\.jsp|\.json)' >> js3.txt &&
 subjs -i alive.txt | tee -a subjs.txt &&
 cat js1.txt js2.txt js3.txt subjs.txt | sort -u | tee -a js-files.txt
+echo ""
 
 #################################################################################################################################
+
+echo ""
+echo ""
+echo "This tool is under developement :)"
