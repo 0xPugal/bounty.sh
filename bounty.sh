267f41dc4c0fb74b00bcb29de9360da767b2ccb9
Pugalarasan <75373225+0xlittleboy@users.noreply.github.com>
2022-10-07 20:32:12 +0530

    Update bounty.sh

--- bounty.sh
+++ bounty.sh
@@ -57,14 +57,8 @@ httpx -l ~/bounty.sh/recon/$1/all.txt -p 80,8080,443,8443,9000,8000 -rl 100 -tim
 
 #Running Portscan
 naabu -l ~/bounty.sh/recon/$1/resolved.txt -port 0-65535 -c 10 -rl 100 -o open-ports.txt
-#############################################################################################################################
 
-echo ""
-sleep 1
-#Subdomain Takeover...
-echo "------------------------"
-echo "...SubDomain Takeover..."
-echo "------------------------"
+#Subdomain Takeover
 python3 ~/bounty.sh/tools/sub404/sub404.py -f ~/bounty.sh/recon/$1/alive.txt -o ~/bounty.sh/recon/$1/sub404.txt
 echo ""
 cd ~/bounty.sh/recon/$1/
@@ -73,59 +67,25 @@ echo ""
 subjack -w alive.txt -ssl -o subjack.txt
 echo ""
 
-################################################################################################################################
-
-echo ""
-sleep 1
 #Vulnerability Scanning - Nuclei...
-echo "----------------------------"
-echo "...Vulnerability Scanning..."
-echo "----------------------------"
-nuclei -l ~/bounty.sh/recon/$1/alive.txt -severity info -o ~/bounty.sh/recon/$1/nuclei-info.txt &&
-nuclei -l ~/bounty.sh/recon/$1/alive.txt -severity low,medium,high,critical -o ~/bounty.sh/recon/$1/nuclei.txt
+nuclei -l ~/bounty.sh/recon/$1/alive.txt -severity info -rl 100 -c 5 -o ~/bounty.sh/recon/$1/nuclei-info.txt;
+nuclei -l ~/bounty.sh/recon/$1/alive.txt -severity low -rl 100 -c 5 -o ~/bounty.sh/recon/$1/nuclei-low.txt;
+nuclei -l ~/bounty.sh/recon/$1/alive.txt -severity medium -rl 100 -c 5 -o ~/bounty.sh/recon/$1/nuclei-medium.txt;
+nuclei -l ~/boutny.sh/recon/$1/alive.txt -severity high -rl 100 -c 5 -o ~/bounty.sh/recon/$1/nuclei-high.txt;
+nuclei -l ~/bounty.sh/recon/$1/alive.txt -severity critical -rl 100 -c 5 -o ~/bounty.sh/recon/$1/nuclei-critical.txt;
 echo ""
 
-################################################################################################################################
-
-echo ""
-sleep 1
 #Smuggler started...
-echo "----------------------"
-echo "...Smuggler started..."
-echo "----------------------"
 cd ~/bounty.sh/recon/$1/
-cat alive.txt | python3 ~/bounty.sh/tools/smuggler/smuggler.py -m POST -q 
+cat alive.txt | python3 ~/bounty.sh/tools/smuggler/smuggler.py -m POST -q;
 cat alive.txt | python3 ~/bounty.sh/tools/smuggler/smuggler.py -m GET -q 
 echo ""
 
-#################################################################################################################################
-
-echo ""
-sleep 1
 #Fuzzing started - ffuf...
-echo "---------------------"
-echo "...Fuzzing started..."
-echo "---------------------"
 cd ~/bounty.sh/recon/$1/
 for URL in $(<alive.txt); do ( ffuf -u "${URL}/FUZZ" -w ~/bounty.sh/wordlists/fuzz.txt -v -mc 200 -ac ); done | tee -a ffuf.txt
 echo ""
 
-############################################################################################################################
-
-echo ""
-sleep 1
-#PortScanning started...
-echo "----------------------"
-echo "...Portscan - Naabu..."
-echo "----------------------"
-cd ~/bounty.sh/recon/$1/
-naabu -l all.txt -p 1-65535 -o portscan.txt
-echo ""
-
-#################################################################################################################################
-
-echo ""
-sleep 1
 #Gathering URLs....
 echo "----------------------------------------------"
 echo "...Gathering URLs - Waybackurls,Gau,Gauplus..."
@@ -138,42 +98,20 @@ cat way.txt gaupl.txt gau.txt | sort -u | tee -a waybackurls.txt
 rm way.txt gaupl.txt gau.txt
 echo ""
 
-#################################################################################################################################
-
-echo ""
-sleep 1
 #CRLF scanning
-echo "----------------------------"
-echo "...CRLF injection scanner..."
-echo "----------------------------"
 cd ~/bounty.sh/tools/CRLF-Injection-Scanner
 python3 crlf.py scan -i ~/bounty.sh/recon/$1/all.txt | egrep -v "No" | tee -a ~/bounty.sh/recon/$1/crlf.txt
 echo ""
 
-##################################################################################################################################
-
-echo ""
-sleep 1
-#Gathering JS Files...
-echo "------------------------"
-echo "...Gathering JS Files..."
-echo "------------------------"
+#Gathering JS Files
 cd ~/bounty.sh/recon/$1/
 cat alive.txt | waybackurls | grep -iE '\.js'|grep -iEv '(\.jsp|\.json)' >> js1.txt &&
-        cat alive.txt | gauplus | grep -iE '\.js'|grep -iEv '(\.jsp|\.json)' >> js2.txt &&
-        cat alive.txt | gau | grep -iE '\.js'|grep -iEv '(\.jsp|\.json)' >> js3.txt &&
+cat alive.txt | gau | grep -iE '\.js'|grep -iEv '(\.jsp|\.json)' >> js3.txt &&
 subjs -i alive.txt | tee -a subjs.txt &&
 cat js1.txt js2.txt js3.txt subjs.txt | sort -u | tee -a js-files.txt
 echo ""
 
-#################################################################################################################################
-
-echo ""
-sleep 1
 #Parameter fuzzing
-echo "-------------------------"
-echo "...ParamSpider started..."
-echo "-------------------------"
 python3 ~/bounty.sh/tools/ParamSpider/paramspider.py -d $1
 echo ""
 
