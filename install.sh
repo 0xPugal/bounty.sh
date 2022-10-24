1a59ecb21ef1879d3bc3da6508045fc1516f9bcc
Pugalarasan <75373225+0xlittleboy@users.noreply.github.com>
2022-10-24 08:39:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,56 +1,48 @@
 #!/bin/bash
 
-cat <<"EOF"
+#Colors
+bold="\e[1m"
+red="\e[31m"
+blue="\e[34m"
+green="\e[32m"
+end="\e[0m"
 
-/===============================================================\
-||   /$$                                       /$$               ||      
-||  | $$                                      | $$               ||
-||  | $$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$  /$$$$$$   /$$   /$$ ||
-||  | $$__  $$ /$$__  $$| $$  | $$| $$__  $$|_  $$_/  | $$  | $$ ||
-||  | $$  \ $$| $$  \ $$| $$  | $$| $$  \ $$  | $$    | $$  | $$ ||
-||  | $$  | $$| $$  | $$| $$  | $$| $$  | $$  | $$ /$$| $$  | $$ ||
-||  | $$$$$$$/|  $$$$$$/|  $$$$$$/| $$  | $$  |  $$$$/|  $$$$$$$ ||
-||  |_______/  \______/  \______/ |__/  |__/   \___/   \____  $$ ||
-||                                                     /$$  | $$ ||
-||                                                    |  $$$$$$/ ||
-||                                                     \______/  ||
-||                                                               ||
-||                                                               ||
-||        Made with ❤️ by Pugalarasan - aka 🧑‍💻 litt1eb0y        ||
-||      Run this script against your target to earn 💰💰💰       ||
- \===============================================================/
-EOF
+echo -e "${bold}${blue}Made with${end} ${bold}${red}<3${end} ${bold}${blue}by Pugalarasan - @0xlittleboy${end}"
 
-mkdir -p ~/bounty.sh/tools
-mkdir -p ~/bounty.sh/wordlists
+cd $HOME
+mkdir -p bounty.sh/tools
 
-cd Tools
+echo -e "${green}${bold}Installing Essentials...${end}"
 
-echo "Installing essentials..."
-sleep 1
-sudo apt update
+sudo apt update -y
 sudo apt upgrade -y
-sudo apt -y install git 
-sudo apt -y install python3 
-sudo apt -y install python3-pip
-sudo apt install python2
+sudo apt install -y gcc
+sudo apt install -y build-essential
+sudo apt install -y git
+sudo apt install -y vim
+sudo apt install -y wget
+sudo apt install -y curl
+sudo apt install -y make
+sudo apt install -y tmux
+sudo apt install -y perl
+sudo apt install -y python3
+sudo apt install -y python3-pip
+sudo apt install -y python
+sudo apt install -y python-pip
+sudo apt install -y libpcap-dev
+sudo apt install -y jq
+sudo apt install -y cargo
+sudo pip3 install uro
 sudo pip3 install colored
-sudo apt-get install jq
-sudo pip3 install tldextract
-sudo apt-get install cargo
-sudo apt install eyewitness
+sudo pip3 install arjun
 sudo pip3 install requests
 suod pip3 install dnspython
 sudo pip3 install argparse
-sudo apt install nmap
-sudo  pip3 install as3nt
-sudo apt install -y libpcap-dev
-sudo pip3 install arjun
-sudo pip3 install uro
+sudo pip3 install tldextract
 
-sleep 1
 
-echo "Installing Golang..."
+
+echo -e "${green}${bold}Installing Golang...${end}"
 sys=$(uname -m)
 LATEST=$(curl -s 'https://go.dev/VERSION?m=text')
 [ $sys == "x86_64" ] && wget https://golang.org/dl/$LATEST.linux-amd64.tar.gz -O golang.tar.gz &>/dev/null || wget https://golang.org/dl/$LATEST.linux-386.tar.gz -O golang.tar.gz &>/dev/null
@@ -60,110 +52,37 @@ echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
 echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> $HOME/.bashrc
 echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.profile
 
-echo ""
 
-echo "Installing tools..."
-
+echo -e "${green}${bold}Installing Tools...${end}"
+cd ~/bounty.sh/tools/
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest;
-echo ""
-
-go get -u github.com/tomnomnom/assetfinder
-echo ""
-
-go install -v github.com/OWASP/Amass/v3/...@master
-echo ""
-
-cd ~/bounty.sh/Tools/
-wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux; chmod +x findomain-linux; sudo cp findomain-linux /usr/local/bin
-echo ""
-
+go get -u github.com/tomnomnom/assetfinder;
+go install -v github.com/OWASP/Amass/v3/...@master;
+wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux; chmod +x findomain-linux; sudo cp findomain-linux /usr/local/bin;
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
-echo ""
-
-cd ~/bounty.sh/Tools/
-wget "https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip" && unzip aquatone_linux_amd64_1.7.0.zip && sudo mv aquatone_linux_amd64_1.7.0 /usr/local/bin
-echo ""
-
+wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux; chmod +x findomain-linux; sudo cp findomain-linux /usr/local/bin
 go install github.com/hakluke/hakrawler@latest;
-echo ""
-
 go install github.com/tomnomnom/waybackurls@latest;
-echo ""
-
 go install github.com/lc/gau/v2/cmd/gau@latest;
-echo ""
-
 go install github.com/003random/getJS@latest;
-echo ""
-
-cd ~/bounty.sh/Tools/
-git clone https://github.com/devanshbatham/ParamSpider.git; cd ParamSpider; pip3 install -r requirements.txt
-echo ""
-
 go install github.com/ffuf/ffuf@latest
-echo ""
-
-cd ~/bounty.sh/Tools/
-git clone https://github.com/s0md3v/Corsy.git
-cd Corsy
-pip3 install -r requirements.txt
-echo ""
-
-cd ~/bounty.sh/Tools/
-git clone https://github.com/MichaelStott/CRLF-Injection-Scanner.git
-cd CRLF-Injection-Scanner
-sudo python3 setup.py install
-echo ""
-
-cd ~/bounty.sh/Tools/
-git clone https://github.com/defparam/smuggler.git; cd smuggler/
-echo ""
-
+git clone https://github.com/s0md3v/Corsy.git; cd Corsy; pip3 install -r requirements.txt; cd ../
+git clone https://github.com/devanshbatham/ParamSpider.git; cd ParamSpider; pip3 install -r requirements.txt; cd ../
+git clone https://github.com/defparam/smuggler.git; cd smuggler/; ../
 go install github.com/tomnomnom/qsreplace@latest;
-echo ""
-
-cd ~/bounty.sh/Tools/
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-echo ""
-
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev; cd ../
 go install github.com/hahwul/dalfox/v2@latest;
-echo ""
-
-cd ~/bounty.sh/Tools/
-git clone https://github.com/dwisiswant0/findom-xss.git --recurse-submodules
-echo ""
-
-cd ~/bounty.sh/Tools/
+git clone https://github.com/dwisiswant0/findom-xss.git --recurse-submodules; cd ../
 git clone https://github.com/internetwache/GitTools.git
-echo ""
-
-cd ~/bounty.sh/Tools/
 git clone https://github.com/SharonBrizinov/s3viewer.git
-echo ""
-
 go get github.com/haccer/subjack
-echo ""
-
 go get github.com/Ice3man543/SubOver
-echo ""
-
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
-echo ""
+git clone https://github.com/edoardottt/cariddi.git; cd cariddi; go get; make linux; cd ../
+go install github.com/tomnomnom/httprobe@latest;
+git clone https://github.com/epinna/tplmap.git; cd tplmap/; chmod +x tplmap.py; cd ..;
+GO111MODULE=on go install github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest;
+git clone https://github.com/s0md3v/XSStrike.git; cd XSStrike; pip3 install -r requirements.txt; cd ../;
 
-cd ~/bounty.sh/Tools/
-git clone https://github.com/edoardottt/cariddi.git; cd cariddi; go get; make linux
-echo ""
 
-go install github.com/tomnomnom/httprobe@latest
-echo ""
-
-echo "###########################################################################################"
-echo "###########################################################################################"
-echo ""
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Happy Hacking~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo ""
-echo "###########################################################################################"
-echo ""
-echo "*****************************This Tool is in under Devepement******************************"
-echo ""
-echo "###########################################################################################"
+echo "${bold}${red}This Tool is in under Devepement${end}"
