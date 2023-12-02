5a726ff620124df4e739449b2ce5b1ea1bbbc040
Pugalarasan <75373225+0xPugazh@users.noreply.github.com>
2023-12-02 13:02:07 +0530

    Update bounty.sh

--- bounty.sh
+++ bounty.sh
@@ -21,7 +21,7 @@ amass enum -passive -d $1 | anew /root/bounty.sh/output/$1/subs.txt
 findomain --target $1 --quiet | anew /root/bounty.sh/output/$1/subs.txt
 cat /root/bounty.sh/output/$1/subs.txt | naabu -top-ports full -silent | anew /root/bounty.sh/output/$1/open-ports.txt
 cat /root/bounty.sh/output/$1/open-ports.txt | httpx -silent | anew /root/bounty.sh/output/$1/alive.txt                                                                                                                                                                                                                                              
-for i in $(cat /root/bounty.sh/output/$1/alive.txt); do xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho $(date +"%T").html ; done 
+for i in $(cat /root/bounty.sh/output/$1/alive.txt); do xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho /root/bounty.sh/output/$1/xray/$(date +"%T").html ; done
 cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity critical -etags ssl | anew /root/bounty.sh/output/$1/nuclei_critical.txt
 cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity high -etags ssl | anew /root/bounty.sh/output/$1/nuclei_high.txt
 cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity medium -etags ssl | anew /root/bounty.sh/output/$1/nuclei_medium.txt
