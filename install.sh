2d1da7e13e8bb8ce3625cf15f8262bee0bdf8a64
Pugalarasan <75373225+0xPugazh@users.noreply.github.com>
2023-12-02 13:37:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ go install -v github.com/owasp-amass/amass/v3/...@master
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+go install -v github.com/tomnomnom/anew@latest
 
 ## installing xray
 wget https://github.com/chaitin/xray/releases/download/1.9.11/xray_linux_amd64.zip
