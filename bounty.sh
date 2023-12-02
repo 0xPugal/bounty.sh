641e4283b9850309dbd56c4f4c2e3ce795e839b3
Pugalarasan <75373225+0xPugazh@users.noreply.github.com>
2023-12-02 18:26:54 +0530

    Update bounty.sh

--- bounty.sh
+++ bounty.sh
@@ -13,18 +13,27 @@ EOF
 
 domain=$1
 mkdir /root/bounty.sh/output/bounty.sh/output/$1
+mkdir /root/bounty.sh/output/bounty.sh/output/$1/nuclei
 mkdir /root/bounty.sh/output/bounty.sh/output/$1/xray
 
+## Subdomain discovery
 subfinder -d $1 -silent -all | anew /root/bounty.sh/output/$1/subs.txt
 assetfinder -subs-only $1 | anew /root/bounty.sh/output/$1/subs.txt
 amass enum -passive -d $1 | anew /root/bounty.sh/output/$1/subs.txt
-findomain --target $1 --quiet | anew /root/bounty.sh/output/$1/subs.txt
+
+## Port scanning
 cat /root/bounty.sh/output/$1/subs.txt | naabu -top-ports full -silent | anew /root/bounty.sh/output/$1/open-ports.txt
-cat /root/bounty.sh/output/$1/open-ports.txt | httpx -silent | anew /root/bounty.sh/output/$1/alive.txt                                                                                                                                                                                                                                              
+
+## HTTP probing
+cat /root/bounty.sh/output/$1/open-ports.txt | httpx -silent | anew /root/bounty.sh/output/$1/alive.txt         
+
+## Vuln Scanning
+# Xray
 for i in $(cat /root/bounty.sh/output/$1/alive.txt); do xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho /root/bounty.sh/output/$1/xray/$(date +"%T").html ; done
-cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity critical -etags ssl | anew /root/bounty.sh/output/$1/nuclei_critical.txt
-cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity high -etags ssl | anew /root/bounty.sh/output/$1/nuclei_high.txt
-cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity medium -etags ssl | anew /root/bounty.sh/output/$1/nuclei_medium.txt
-cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity low -etags ssl | anew /root/bounty.sh/output/$1/nuclei_low.txt
+# Nuclei
+cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity critical -etags ssl | anew /root/bounty.sh/output/$1/nuclei/critical.txt
+cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity high -etags ssl | anew /root/bounty.sh/output/$1/nuclei/high.txt
+cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity medium -etags ssl | anew /root/bounty.sh/output/$1/nuclei/medium.txt
+cat /root/bounty.sh/output/$1/alive.txt | nuclei -t /root/nuclei-templates -severity low -etags ssl | anew /root/bounty.sh/output/$1/nuclei/low.txt
 
 ## Inspired from https://medium.com/@0xelkot/how-i-get-10-sqli-and-30-xss-via-automation-tool-cebbd9104479
