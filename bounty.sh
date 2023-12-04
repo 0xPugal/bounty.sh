ee756035dbf2ce61943de873e37fef3379b1b8ee
Pugalarasan <75373225+0xPugazh@users.noreply.github.com>
2023-12-04 21:13:43 +0530

    Update bounty.sh

--- bounty.sh
+++ bounty.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 ## colors
-BLUE='\033[0;34m'
 BOLD="\e[1m"
 CYAN='\033[0;36m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 
-echo -e "${BOLD}${CYAN}     ____                    __               __      ${NC}"
-echo -e "${BOLD}${CYAN}    / __ )____  __  ______  / /___  __  _____/ /_     ${NC}"
-echo -e "${BOLD}${CYAN}   / __  / __ \/ / / / __ \/ __/ / / / / ___/ __ \    ${NC}"
-echo -e "${BOLD}${CYAN}  / /_/ / /_/ / /_/ / / / / /_/ /_/ / (__  ) / / /    ${NC}"
-echo -e "${BOLD}${CYAN} /_____/\____/\__,_/_/ /_/\__/\__, (_)____/_/ /_/     ${NC}"
-echo -e "${BOLD}${CYAN}                            /____/                     ${NC}"
-echo -e "${BOLD}${CYAN} Bug Bounty automation script - $$$ by @0xPugazh      ${NC}"
+echo -e "$CYAN${BOLD}     ____                    __               __      ${NC}"
+echo -e "$CYAN${BOLD}    / __ )____  __  ______  / /___  __  _____/ /_     ${NC}"
+echo -e "$CYAN${BOLD}   / __  / __ \/ / / / __ \/ __/ / / / / ___/ __ \    ${NC}"
+echo -e "$CYAN${BOLD}  / /_/ / /_/ / /_/ / / / / /_/ /_/ / (__  ) / / /    ${NC}"
+echo -e "$CYAN${BOLD} /_____/\____/\__,_/_/ /_/\__/\__, (_)____/_/ /_/     ${NC}"
+echo -e "$CYAN${BOLD}                            /____/                     ${NC}"
+echo -e "$CYAN${BOLD} Bug Bounty automation script - $$$ by @0xPugazh      ${NC}"
 echo ""
 
 echo " "
 help() {
-    echo -e "${BOLD}${CYAN}Usage:${NC}"
+    echo -e "$CYAN${BOLD}Usage:${NC}"
     echo -e "${BOLD}--help            Shows the help menu${NC}"
     echo -e "${BOLD}--vuln1           Subs + port + alive + xray + nuclei${NC}"
     echo -e "${BOLD}--vuln2           Subs + alive + params + xray + nuclei_fuzzing${NC}"
@@ -41,18 +40,18 @@ mkdir -p /root/bounty.sh/output/"$domain"/xray
 mkdir -p /root/bounty.sh/output/"$domain"/nuclei
 
 vuln1() {
-    echo -e "${BOLD}${BLUE}Subdomain Enumeration...${NC}"
+    echo -e "$CYAN${BOLD}Subdomain Enumeration...${NC}"
     subfinder -d $domain -silent -all | anew /root/bounty.sh/output/$domain/subs.txt
     assetfinder -subs-only $domain | anew /root/bounty.sh/output/$domain/subs.txt
     amass enum -passive -d $domain | anew /root/bounty.sh/output/$domain/subs.txt
 
-    echo -e "${BOLD}${BLUE}Port Scanning...${NC}"
+    echo -e "$CYAN${BOLD}Port Scanning...${NC}"
     cat /root/bounty.sh/output/$domain/subs.txt | naabu -top-ports full -silent | anew /root/bounty.sh/output/$domain/open-ports.txt
     
-    echo -e "${BOLD}${BLUE}HTTP Probing...${NC}"
+    echo -e "$CYAN${BOLD}HTTP Probing...${NC}"
     cat /root/bounty.sh/output/$domain/open-ports.txt | httpx -silent | anew /root/bounty.sh/output/$domain/alive.txt   
 
-    echo -e "${BOLD}${BLUE}Vulnerability Scanning...${NC}"
+    echo -e "$CYAN${BOLD}Vulnerability Scanning...${NC}"
     for i in $(cat /root/bounty.sh/output/$domain/alive.txt); do 
         xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho /root/bounty.sh/output/$domain/xray/$(date +"%T").html
     done
@@ -63,18 +62,18 @@ vuln1() {
 }
 
 vuln2() {
-    echo -e "${BOLD}${BLUE}Subdomain Enumeration...${NC}"
+    echo -e "$CYAN${BOLD}Subdomain Enumeration...${NC}"
     subfinder -d $domain -silent -all | anew /root/bounty.sh/output/$domain/subs.txt
     assetfinder -subs-only $domain | anew /root/bounty.sh/output/$domain/subs.txt
     amass enum -passive -d $domain | anew /root/bounty.sh/output/$domain/subs.txt
 
-    echo -e "${BOLD}${BLUE}HTTP Probing...${NC}"
+    echo -e "$CYAN${BOLD}HTTP Probing...${NC}"
     cat /root/bounty.sh/output/$domain/subs.txt | httpx -silent | anew /root/bounty.sh/output/$domain/alive.txt
 
-    echo -e "${BOLD}${BLUE}Parameters finding...${NC}"
+    echo -e "$CYAN${BOLD}Parameters finding...${NC}"
     paramspider -l /root/bounty.sh/output/$domain/alive.txt && mv results /root/bounty.sh/output/$domain/ 
 
-    echo -e "${BOLD}${BLUE}Vulnerability Scanning...${NC}"
+    echo -e "$CYAN${BOLD}Vulnerability Scanning...${NC}"
     cat /root/bounty.sh/output/$domain/results/$domain.txt | xargs -I @ sh -c 'xray_linux_amd64 ws --url-list @ --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho /root/bounty.sh/output/$domain/xray/$(date +"%T").html'
     cat /root/bounty.sh/output/$domain/results/$domain.txt | nuclei -t /root/nuclei-templates -severity critical -etags ssl | anew /root/bounty.sh/output/$domain/nuclei/critical.txt
     cat /root/bounty.sh/output/$domain/results/$domain.txt | nuclei -t /root/nuclei-templates -severity high -etags ssl | anew /root/bounty.sh/output/$domain/nuclei/high.txt
